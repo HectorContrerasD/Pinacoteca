3897fb2c7b44d82be4e0e01a6e97371880b384d2
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2023-11-24 14:21:42 -0600

    Add project files.

---
+++ Pinacoteca/Models/Entities/PinacotecabdContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pinacoteca.Models.Entities;
+
+public partial class PinacotecabdContext : DbContext
+{
+    public PinacotecabdContext()
+    {
+    }
+
+    public PinacotecabdContext(DbContextOptions<PinacotecabdContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Coleccion> Coleccion { get; set; }
+
+    public virtual DbSet<Cuadro> Cuadro { get; set; }
+
+    public virtual DbSet<Pintor> Pintor { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;password=root;user=root;database=pinacotecabd", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Coleccion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("coleccion");
+
+            entity.Property(e => e.Ciudad).HasMaxLength(80);
+            entity.Property(e => e.Direccion).HasColumnType("text");
+            entity.Property(e => e.Nombre).HasMaxLength(80);
+            entity.Property(e => e.Pais).HasMaxLength(80);
+        });
+
+        modelBuilder.Entity<Cuadro>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("cuadro");
+
+            entity.HasIndex(e => e.IdColeccion, "IdColeccion_idx");
+
+            entity.HasIndex(e => e.IdPintor, "IdPintor_idx");
+
+            entity.Property(e => e.Descripcion).HasColumnType("text");
+            entity.Property(e => e.Dimensiones).HasMaxLength(15);
+            entity.Property(e => e.Imagen).HasMaxLength(20);
+            entity.Property(e => e.Tecnica).HasMaxLength(50);
+            entity.Property(e => e.TituloCuadro).HasMaxLength(100);
+
+            entity.HasOne(d => d.IdColeccionNavigation).WithMany(p => p.Cuadro)
+                .HasForeignKey(d => d.IdColeccion)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("IdColeccion");
+
+            entity.HasOne(d => d.IdPintorNavigation).WithMany(p => p.Cuadro)
+                .HasForeignKey(d => d.IdPintor)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("IdPintor");
+        });
+
+        modelBuilder.Entity<Pintor>(entity =>
+        {
+            entity.HasKey(e => e.IdPintor).HasName("PRIMARY");
+
+            entity.ToTable("pintor");
+
+            entity.Property(e => e.Biografia).HasColumnType("text");
+            entity.Property(e => e.Ciudad).HasMaxLength(80);
+            entity.Property(e => e.FechaFallecimiento).HasMaxLength(15);
+            entity.Property(e => e.FechaNacimiento).HasMaxLength(15);
+            entity.Property(e => e.Imagen).HasMaxLength(20);
+            entity.Property(e => e.Nombre).HasMaxLength(80);
+            entity.Property(e => e.Pais).HasMaxLength(80);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Pinacoteca/Models/Entities/Pintor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pinacoteca.Models.Entities;
+
+public partial class Pintor
+{
+    public int IdPintor { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Pais { get; set; } = null!;
+
+    public string Ciudad { get; set; } = null!;
+
+    public string? FechaNacimiento { get; set; }
+
+    public string? FechaFallecimiento { get; set; }
+
+    public string Biografia { get; set; } = null!;
+
+    public string Imagen { get; set; } = null!;
+
+    public virtual ICollection<Cuadro> Cuadro { get; set; } = new List<Cuadro>();
+}
